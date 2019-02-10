34af6831dda504ab7734c61f7e9a201e3d53393b
David Pearl <davidtpearl@gmail.com>
2019-02-11 10:48:55 +1100

    JSON wizardry for decks, games, users

---
+++ app/views/decks/_deck.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! deck, :id, :name, :sfw
+json.url deck_url(deck, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games
+  resources :users
+  resources :decks
 
   get '/admin' => 'admin#index'
 end
