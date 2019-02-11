53caa56c81163b90ecc48a366e90fec7dd9e165e
NC <nick.e.christiansen@gmail.com>
2019-02-11 21:45:57 +1100

    fix what I broke

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,37 +1,75 @@
 class RoundsController < ApplicationController
+  before_action :set_round, only: [:show, :edit, :update, :destroy]
 
+  # GET /rounds
+  # GET /rounds.json
   def index
     @rounds = Round.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json:@rounds}
-    end
   end
 
+  # GET /rounds/1
+  # GET /rounds/1.json
   def show
-    @round = Round.find params[:id]
+    # json for rounds/:id
   end
 
+  # GET /rounds/new
   def new
     @round = Round.new
   end
 
-  def create
-    @round = Round.create user_params
+  # GET /rounds/1/edit
+  def edit
+  end
 
-  #  redirect_to @user
+  # POST /rounds
+  # POST /rounds.json
+  def create
+    @round = Round.new(round_params)
+
+    respond_to do |format|
+      if @round.save
+        format.html { redirect_to @round, notice: 'Round was successfully created.' }
+        format.json { render :show, status: :created, location: @round }
+      else
+        format.html { render :new }
+        format.json { render json: @round.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /rounds/1
+  # PATCH/PUT /rounds/1.json
+  def update
+    respond_to do |format|
+      if @round.update(round_params)
+        format.html { redirect_to @round, notice: 'Round was successfully updated.' }
+        format.json { render :show, status: :ok, location: @round }
+      else
+        format.html { render :edit }
+        format.json { render json: @round.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /rounds/1
+  # DELETE /rounds/1.json
+  def destroy
+    @round.destroy
+    respond_to do |format|
+      format.html { redirect_to rounds_url, notice: 'Round was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
-  # Update these params to reflect user params
-    def round_params
-<<<<<<< HEAD
-      params.require(:post).permit(:game_id, :number, :likes, :instance_id, :winner_id, :master_id, :winning_card_id)
-=======
-      params.require(:round).permit(:game_id, :master_id, :number)
->>>>>>> f4582b67300556040ce8dc89c99c954b52c052fa
+    # Use callbacks to share common setup or constraints between actions.
+    def set_round
+      @round = Round.find(params[:id])
     end
 
-
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def round_params
+      params.require(:round).permit(:game_id, :number, :master_id)
+    end
 end
