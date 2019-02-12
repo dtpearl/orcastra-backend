8c74cdf40d1f169f354abd377eedfc1d4b57d714
NC <nick.e.christiansen@gmail.com>
2019-02-12 20:22:03 +1100

    add associations for new round_id column to burns model

--- app/models/burn.rb
+++ app/models/burn.rb
@@ -2,4 +2,5 @@ class Burn < ApplicationRecord
   belongs_to :game, optional: true
   belongs_to :response, optional: true
   belongs_to :user, optional: true
+  belongs_to :round, optional: true
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,5 @@
 class Round < ApplicationRecord
   belongs_to :game, optional: true
   belongs_to :instance, optional: true
+  has_many :burns
 end
